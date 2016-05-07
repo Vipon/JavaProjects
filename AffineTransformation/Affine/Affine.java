b6d7d6684ac4aee5a5a8bbf3ed1ff5d80d120b7f
Konychev Valery <valerakonychev@gmail.com>
2016-05-08 02:40:54 +0300

    Fixed Bug in AffineTransformation.

--- AffineTransformation/Affine/Affine.java
+++ AffineTransformation/Affine/Affine.java
@@ -3,27 +3,27 @@ package Affine;
 import Matrix.*;
 
 public class Affine{
-    
+
     public enum Mode {NEAREST_NEIGHBOR, ARITHMETIC_AVERAGE};
     private Mode mode;
-    
+
     private int curWidth;
     private int curHeight;
     private int newWidth;
     private int newHeight;
-    
+
     private double rad;
     private double koeff;
-    
+
     private Matrix rotateMatrix;
     private Matrix arotateMatrix;
-    
+
     public Affine(double angle, double koeff, Mode mode) {
-        
+
         //this.angle = angle;
         this.koeff = koeff;
         this.mode = mode;
-        
+
         rad = Math.toRadians(angle);
         double[][] rotM = new double[2][2];
         rotM[0][0] = Math.cos(rad);
@@ -31,23 +31,23 @@ public class Affine{
         rotM[1][0] = Math.sin(rad);
         rotM[1][1] = Math.cos(rad);
         rotateMatrix = new Matrix(rotM);
-        
+
         rotM[0][1] = Math.sin(rad);
         rotM[1][0] = -Math.sin(rad);
         arotateMatrix = new Matrix(rotM);
     }
-    
+
     public Matrix transform(Matrix m) {
-        
+
         curWidth = m.getWigth();
         curHeight = m.getHeight();
-        
+
         double[][] Old = m.getDarray();
         newBounds(Old);
         double[][] New = new double[newHeight][newWidth];
-       
+
         switch(mode) {
-        
+
         case NEAREST_NEIGHBOR:
             nearestNeighbor(Old, New);
             break;
@@ -59,13 +59,13 @@ public class Affine{
         }
         return new Matrix(New);
     }
-    
+
     private void newBounds(double[][] Old) {
-        
+
         Matrix oldPoint = new Matrix(2,1);
         Matrix newPoint = new Matrix(2,1);
         double[][] Point = new double[2][1];
-        
+
         double[] p;
         double x;
         double y;
@@ -91,68 +91,68 @@ public class Affine{
         y = p[1];
         if(Math.abs(y) > maxY) maxY = Math.abs(y);
         if(Math.abs(x) > maxX) maxX = Math.abs(x);
-                
+
         newWidth = 1 + (int) Math.ceil(2*maxX);
         newHeight = 1 + (int) Math.ceil(2*maxY);
         newWidth *= koeff;
         newHeight *= koeff;
     }
-    
+
     private void nearestNeighbor(double[][] Old, double[][] New) {
-        
+
         int oldX = 0;
         int oldY = 0;
         double[] p;
         double[][] Point = new double[2][1];
         Matrix oldPoint = new Matrix(2,1);
         Matrix newPoint = new Matrix(2,1);
-        
+
         for(int i = 0; i < newHeight; ++i)
-            for(int j = 0; j < newWidth; ++j)  
+            for(int j = 0; j < newWidth; ++j)
                 if(New[i][j] == 0) {
-                    
+
                     Point[0][0] = (j - newWidth/2)/koeff;
                     Point[1][0] = (i - newHeight/2)/koeff;
                     newPoint.setDarray(Point);
                     oldPoint = arotateMatrix.mul(newPoint);
                     p = oldPoint.getArray();
-                    
+
                     oldX = (int)(p[0] + curWidth/2);
                     oldY = (int)(p[1] + curHeight/2);
-                    if((oldX < curWidth) && (oldX >= 0) && (oldY < curHeight) && (oldY >= 0))                         
+                    if((oldX < curWidth) && (oldX >= 0) && (oldY < curHeight) && (oldY >= 0))
                         New[i][j] = Old[oldY][oldX];
                 }
     }
-    
+
     private void arithmeticAverage(double[][] Old, double[][] New) {
-        
+
         int oldX1 = 0;
         int oldY1 = 0;
         int oldX2 = 0;
         int oldY2 = 0;
-        
+
         double[] p;
         double[][] Point = new double[2][1];
         Matrix oldPoint = new Matrix(2,1);
         Matrix newPoint = new Matrix(2,1);
-        
+
         for(int i = 0; i < newHeight; ++i)
-            for(int j = 0; j < newWidth; ++j)  
+            for(int j = 0; j < newWidth; ++j)
                 if(New[i][j] == 0) {
-                    
+
                     Point[0][0] = ((double)j - (double)newWidth/2)/koeff;
                     Point[1][0] = ((double)i - (double)newHeight/2)/koeff;
                     newPoint.setDarray(Point);
                     oldPoint = arotateMatrix.mul(newPoint);
                     p = oldPoint.getArray();
-                    
+
                     double x = (p[0] + (double)curWidth/2);
                     double y = (p[1] + (double)curHeight/2);
                     oldX1 = (int)(p[0] + (double)curWidth/2);
                     oldY1 = (int)(p[1] + (double)curHeight/2);
                     oldX2 = (int)Math.ceil((p[0] + (double)curWidth/2));
                     oldY2 = (int)Math.ceil((p[1] + (double)curHeight/2));
-                    
+
                     int R, G, B;
                     int R0, G0, B0;
                     int R1, G1, B1;
@@ -160,42 +160,41 @@ public class Affine{
                     int R3, G3, B3;
                     int Head = 0xFF;
                     if ((oldX2 < curWidth) && (oldX1 >= 0) && (oldY2 < curHeight) && (oldY1 >= 0)) {
-                        
+
                         B0 = ((int)(Old[oldY1][oldX1])) & 0x000000FF;
                         B1 = ((int)(Old[oldY1][oldX2])) & 0x000000FF;
                         B2 = ((int)(Old[oldY2][oldX1])) & 0x000000FF;
                         B3 = ((int)(Old[oldY2][oldX2])) & 0x000000FF;
-                        
+
                         G0 = (((int)(Old[oldY1][oldX1])) >> 8) & 0x000000FF;
                         G1 = (((int)(Old[oldY1][oldX2])) >> 8) & 0x000000FF;
                         G2 = (((int)(Old[oldY2][oldX1])) >> 8) & 0x000000FF;
                         G3 = (((int)(Old[oldY2][oldX2])) >> 8) & 0x000000FF;
-                        
+
                         R0 = (((int)(Old[oldY1][oldX1])) >> 16) & 0x000000FF;
                         R1 = (((int)(Old[oldY1][oldX2])) >> 16) & 0x000000FF;
                         R2 = (((int)(Old[oldY2][oldX1])) >> 16) & 0x000000FF;
                         R3 = (((int)(Old[oldY2][oldX2])) >> 16) & 0x000000FF;
-                        
+
                         if ((oldX2 == oldX1) && (oldY1 == oldY2)) {
-                            
+
                             New[i][j] = ((Head<<24) + (R1<<16) + (G1<<8) + B1);
-                        }
-                        if (oldX2 == oldX1) {
-                            
+                        } else if (oldX2 == oldX1) {
+
                             B =  (int)(B0*((double)oldY2 - y) + B2*(y - (double)oldY1)) & 0x000000FF;
                             G =  (int)(G0*((double)oldY2 - y) + G2*(y - (double)oldY1)) & 0x000000FF;
                             R =  (int)(R0*((double)oldY2 - y) + R2*(y - (double)oldY1)) & 0x000000FF;
-                            
+
                             New[i][j] = ((Head<<24) + (R<<16) + (G<<8) + B);
                         } else if (oldY1 == oldY2) {
-  
+
                             B =  (int)(B0*((double)oldX2 - x) + B1*(x - (double)oldX1)) & 0x000000FF;
                             G =  (int)(G0*((double)oldX2 - x) + G1*(x - (double)oldX1)) & 0x000000FF;
                             R =  (int)(R0*((double)oldX2 - x) + R1*(x - (double)oldX1)) & 0x000000FF;
-                            
+
                             New[i][j] = ((Head<<24) + (R<<16) + (G<<8) + B);
                         } else {
- 
+
                             B =  (int)((B0*((double)oldX2 - x)*((double)oldY2 - y) +
                                         B1*(x - (double)oldX1)*((double)oldY2 - y) +
                                         B2*((double)oldX2 - x)*(y - (double)oldY1) +
@@ -208,11 +207,11 @@ public class Affine{
                                         R1*(x - (double)oldX1)*((double)oldY2 - y) +
                                         R2*((double)oldX2 - x)*(y - (double)oldY1) +
                                         R3*(x - (double)oldX1)*(y - (double)oldY1))) & 0x000000FF;
-                           
+
                             New[i][j] = ((Head<<24) + (R<<16) + (G<<8) + B);
                         }
                     }
                 }
-               
+
     }
 }
